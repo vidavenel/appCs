14fad5cac012620d5466670f5ac17dd6ac4a0090
vidavenel <vincent_mil@hotmail.fr>
2016-09-25 11:35:21 +0200

    fix envoy file

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -6,15 +6,15 @@ php artisan down
 {{-- on supprime les caches --}}
 php artisan route:clear
 php artisan cache:clear
-{{-- on defait toute les migration --}}
-php artisan migrate:reset
+
 {{-- on pull le depot git --}}
 git checkout -- composer.lock
 git pull
 {{-- on update les dependances --}}
 composer install
-{{-- on refait les migrations avec les seeds --}}
-php artisan migrate --seed
+{{-- on joue les migrations --}}
+php artisan migrate
+
 {{-- en remet l'application en ligne --}}
 php artisan up
 @endtask
